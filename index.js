2d0d55ca0815a5b492b6aa165020a6b0337c7ecd
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-07-11 10:50:39 -0700

    wrote shift function

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ function push(arr, item) {
   const number = arr.length + 1;
   console.log(number);
   arr[number] = item;
-  return arr;
+  return arr.length;
 }
 
-module.exports = { push };
+function shift(arr) {
+    const number = arr[0];
+    
+    for (let i = 0; i < arr.length; i++) {
+        //console.log(arr[i]);
+        arr[i] = arr[i + 1];
+    }
+
+    arr.length = arr.length -1;
+
+
+    return number;
+  }
+
+module.exports = { push, shift };
 
 // Read element by index | `const number = arr[i]`
 // Set element by index | `arr[i] = arr[i + 1]`
