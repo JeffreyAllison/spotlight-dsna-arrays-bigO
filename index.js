2698a9f88893daa4e464bd536359228c7987cca7
JeffAllison <itsjallison@gmail.com>
2022-07-11 10:17:09 -0700

    attempt at push array function

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function push(arr, item) {}
+function push(arr, item) {
+  const number = arr.length + 1;
+  console.log(number);
+  arr[number] = item;
+  return arr;
+}
 
 module.exports = { push };
+
+// Read element by index | `const number = arr[i]`
+// Set element by index | `arr[i] = arr[i + 1]`
+// Read array length | `for(let i = 0; i < arr.length; i++) {`
+// Set array length | `arr.length = arr.length - 1`
--- index.test.js
+++ index.test.js
@@ -1,10 +1,10 @@
-const { push } = require("./index.js");
+const { push } = require('./index.js');
 
-describe("push", () => {
-  it("should add an element to an array", () => {
-    const arr = ["a", "b", "c"];
-    expect(push(arr, "d")).toBe(4);
+describe('push', () => {
+  it('should add an element to an array', () => {
+    const arr = ['a', 'b', 'c'];
+    expect(push(arr, 'd')).toBe(4);
     expect(arr.length).toBe(4);
-    expect(arr[3]).toBe("d");
+    expect(arr[3]).toBe('d');
   });
 });
